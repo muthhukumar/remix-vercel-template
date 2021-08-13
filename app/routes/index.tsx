a995f0980bb408c4d247a0addee7ca591fcf0f65
Muthukumar <muthukumarperiyasamy.mk@gmail.com>
2021-08-13 22:08:44 +0530

    feat: nothing

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -23,9 +23,6 @@ export default function Index() {
   return (
     <div style={{ textAlign: 'center', padding: 20 }}>
       <h2>Welcome to Remix!</h2>
-      <p>
-        <a href="https://remix.run/dashboard/docs">Check out the docs</a> to get started.
-      </p>
       <p>Message from the loader: {data.message}</p>
     </div>
   )
